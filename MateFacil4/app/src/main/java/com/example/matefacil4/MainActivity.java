a352eb81d9a5e0b660ae543b4e40159fcb8bb322
Mikolenn <castroariasricardo@gmail.com>
2020-12-07 01:06:41 -0600

    Preview working

--- MateFacil4/app/src/main/java/com/example/matefacil4/MainActivity.java
+++ MateFacil4/app/src/main/java/com/example/matefacil4/MainActivity.java
@@ -9,6 +9,7 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -30,8 +31,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // ImageView para mostrar la imagen capturada
         img = (ImageView)findViewById(R.id.imageView);
 
+        // Permisos iniciales para acceder a la cámara y almacenamiento
         if (ContextCompat.checkSelfPermission(MainActivity.this,
                 Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED &&
                 ActivityCompat.checkSelfPermission(MainActivity.this,
@@ -43,9 +46,10 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    // Variable para guardar la dirección de memoria de la imagen capturada
+    String currentPhotoPath;
 
-    String mCurrentPhotoPath;
-
+    // Se crea el nombre único y detergente, para la imagen actual
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
@@ -58,12 +62,12 @@ public class MainActivity extends AppCompatActivity {
         );
 
         // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.getAbsolutePath();
+        currentPhotoPath = image.getAbsolutePath();
         return image;
     }
 
 
-    static final int REQUEST_TAKE_PHOTO = 1;
+    static final int REQUEST_IMAGE_CAPTURE = 1;
 
     public void takePicture(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -71,33 +75,63 @@ public class MainActivity extends AppCompatActivity {
         if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
             // Create the File where the photo should go
             File photoFile = null;
+
             try {
                 photoFile = createImageFile();
             } catch (IOException ex) {
                 // Error occurred while creating the File
             }
+
+
             // Continue only if the File was successfully created
             if (photoFile != null) {
+
+                // Dirección completa donde se guarda la imagen
                 Uri photoURI = FileProvider.getUriForFile(this,
-                        "com.example.android.fileprovider",
-                        photoFile);
+                                                        "com.example.android.fileprovider",
+                                                        photoFile);
+
+                // Intent para añadir el nombre completo y la dirección de la imagen
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+
+                // Se guarda la imagen en galería y se inicia la función impícita onActivityResult
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
     }
 
-    /*
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            img.setImageBitmap(imageBitmap);
+
+            File file = new File(currentPhotoPath);
+            Bitmap bitmap = null;
+
+            // Matriz para realizar el giro de la imagen
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+
+            try {
+                // Se recupera la captura de la galería, para mostrarla como un bitmap en la app
+                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(),
+                        Uri.fromFile(file));
+            } catch (Exception error){
+                // Placeholder
+            }
+
+            // Se ajusta el tamaño
+            Bitmap sBitmap = Bitmap.createScaledBitmap(bitmap,
+                    750, 750, false);
+
+            // Se rota, porque si no, sale girada a la izquiera
+            Bitmap rBitmap = Bitmap.createBitmap(sBitmap, 0, 0,
+                    sBitmap.getWidth(), sBitmap.getHeight(), matrix, true);
+
+            // Se muestra en la vista previa del layout
+            img.setImageBitmap(rBitmap);
         }
     }
-    */
 
 }
