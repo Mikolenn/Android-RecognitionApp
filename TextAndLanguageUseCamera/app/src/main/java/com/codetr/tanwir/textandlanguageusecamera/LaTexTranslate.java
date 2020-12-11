707c661da232b3bdcdd39c1dde57d1f8c6c5677b
Mikolenn <castroariasricardo@gmail.com>
2020-12-10 19:56:31 -0600

    Coments added

--- TextAndLanguageUseCamera/app/src/main/java/com/codetr/tanwir/textandlanguageusecamera/LaTexTranslate.java
+++ TextAndLanguageUseCamera/app/src/main/java/com/codetr/tanwir/textandlanguageusecamera/LaTexTranslate.java
@@ -8,17 +8,21 @@ public class LaTexTranslate {
      */
     public static String translateEquation(String equation, String result){
 
+        // Homogenize the read variable
         equation = equation.replaceAll("X", "x");
+        // Puts the opening of the LaTex environment and ident the next line
         String translated = "\\begin{align*} \n    ";
 
+        // Translation of the equation recognized by the OCR algorithm
         for (int i=0; i < equation.length(); i++){
 
             String temp = "";
             
+            // Read the next value, looking for a multiplication or division
             if ( i < (equation.length() - 2))
                 temp = equation.substring(i + 1, i + 2);
 
-
+            // Case for the division symbol
             if ( temp.contains("/") ){
 
                 translated = translated + "\\frac{" + equation.substring(i, i+1) + "}{" +
@@ -26,7 +30,7 @@ public class LaTexTranslate {
 
                 i += 2;
             }
-            else {
+            else {  // Case for the multiplication symbol
 
                 if ( temp.contains("*") ){
 
@@ -40,6 +44,7 @@ public class LaTexTranslate {
             }
         }
 
+        // Build the final result for the equations
         if( equation.contains("=") ){
 
             if ( result.equals("Soluciones infinitas") )
@@ -52,11 +57,11 @@ public class LaTexTranslate {
             translated = translated + " \\\\ \n    x = " + result;
 
         }
-        else {
+        else {  // Build the final result for the expressions
 
             translated = translated + " = " + result;
         }
-
+        // Puts the closing of the LaTex environment
         translated = translated + "\n \\end{align*}";
         return translated;
     }
